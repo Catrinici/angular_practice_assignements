716452ecf3c2fc87772a532f43a379635159cc48
Catrinici <catrinici.antonina@gmail.com>
2019-12-17 09:54:31 -0800

    Typescript bikes-oop

--- typescript_bikes.js
+++ typescript_bikes.js
@@ -21,52 +21,43 @@ Which methods can return this in order to allow chaining methods?
 
 For assignment submission, upload a ".ts" file with the contents of the TypeScript Playground.
 */
-class Bike {
-	price: number;
-	max_speed: string;
-	miles: number;
-	constructor(price: number, max_speed: string) {
-		this.price = price;
-		this.max_speed = max_speed;
-		this.miles = 0;
-		console.log("New Bike")
-	}
-
-	displayInfo() {
-		console.log(`Price ${this.price} , Max speed: ${this.max_speed}, Miles: ${this.miles}`)
-	}
-
-	ride() {
-		console.log(`Riding...`);
-		this.miles += 10;
-		return this;
-	}
-
-	reverse() {
-		console.log(`Reversing...`);
-		if (this.miles <= 5) {
-			this.miles = 0;
-			return this;
-		}
-		else {
-			this.miles -= 5;
-			return this;
-		}
-	}
-}
-
+var Bike = /** @class */ (function () {
+    function Bike(price, max_speed) {
+        this.price = price;
+        this.max_speed = max_speed;
+        this.miles = 0;
+        console.log("New Bike");
+    }
+    Bike.prototype.displayInfo = function () {
+        console.log("Price " + this.price + " , Max speed: " + this.max_speed + ", Miles: " + this.miles);
+    };
+    Bike.prototype.ride = function () {
+        console.log("Riding...");
+        this.miles += 10;
+        return this;
+    };
+    Bike.prototype.reverse = function () {
+        console.log("Reversing...");
+        if (this.miles <= 5) {
+            this.miles = 0;
+            return this;
+        }
+        else {
+            this.miles -= 5;
+            return this;
+        }
+    };
+    return Bike;
+}());
 var bike1 = new Bike(3000, "10mph");
 var bike2 = new Bike(2000, "15mph");
 var bike3 = new Bike(1000, "30mph");
-
-console.log(`~Bike 1~`);
+console.log("~Bike 1~");
 bike1.ride().ride().ride().reverse().reverse();
 bike1.displayInfo();
-
-console.log(`~Bike 2~`)
+console.log("~Bike 2~");
 bike2.ride().ride().reverse().reverse();
 bike2.displayInfo();
-
 console.log("~Bike 3~");
 bike3.reverse().reverse().reverse();
 bike3.displayInfo();
